d9cf802eb3d06edb6e27586975d7813a056ae3a3
Yuri <yuri.shimane@gmail.com>
2025-06-25 17:13:22 -0400

    workflow fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,10 +7,17 @@ include("utils.jl")
 include(joinpath(@__DIR__, "../src/HFEM.jl"))
 
 # furnish spice kernels
-spice_dir = ENV["SPICE"]
-furnsh(joinpath(spice_dir, "lsk", "naif0012.tls"))
-furnsh(joinpath(spice_dir, "spk", "de440.bsp"))
-furnsh(joinpath(spice_dir, "pck", "gm_de440.tpc"))
+if !haskey(ENV, "SPICE")
+    spice_dir = joinpath(@__DIR__, "../spice/test")
+    furnsh(joinpath(spice_dir, "naif0012.tls"))
+    furnsh(joinpath(spice_dir, "de440.bsp"))
+    furnsh(joinpath(spice_dir, "gm_de440.tpc"))
+else
+    spice_dir = ENV["SPICE"]
+    furnsh(joinpath(spice_dir, "lsk", "naif0012.tls"))
+    furnsh(joinpath(spice_dir, "spk", "de440.bsp"))
+    furnsh(joinpath(spice_dir, "pck", "gm_de440.tpc"))
+end
 
 
 @testset "Ephemeris interpolation" begin
