c29acd1bc36f0ed6deddde1f8c55b58124f492d7
Yuri <yuri.shimane@gmail.com>
2025-06-28 13:13:07 -0400

    Update HFEM.jl

--- src/HFEM.jl
+++ src/HFEM.jl
@@ -22,8 +22,13 @@ include("eoms/eom_NbodySH_SPICE.jl")
 include("events.jl")
 
 export InterpolatedEphemeris
+export InterpolatedTransformation
 export HFEMParameters
-export eom_Nbody_SPICE!, eom_stm_Nbody_SPICE!, dfdx_Nbody_SPICE 
+
+export eom_Nbody_SPICE!, eom_stm_Nbody_SPICE!, dfdx_Nbody_SPICE
+export eom_Nbody_Interp!, eom_Nbody_Interp, eom_stm_Nbody_Interp!, dfdx_Nbody_Interp
+export eom_stm_Nbody_Interp_fd!, dfdx_Nbody_Interp_fd
+
 export get_trueanomaly_event
 
 end # module HFEM
