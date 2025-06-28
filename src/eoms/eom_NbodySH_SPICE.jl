0b15140f35e9f2bbcbe9d78304be30745771800f
Yuri <yuri.shimane@gmail.com>
2025-06-28 13:13:12 -0400

    Update eom_NbodySH_SPICE.jl

--- src/eoms/eom_NbodySH_SPICE.jl
+++ src/eoms/eom_NbodySH_SPICE.jl
@@ -1,4 +1,4 @@
-"""Equations of motion for N-body problem with spherical harmonics"""
+"""Equations of motion for N-body problem with spherical harmonics using SPICE"""
 
 
 """Right-hand side of N-body equations of motion compatible with `DifferentialEquations.jl`"""
@@ -17,21 +17,18 @@ function eom_NbodySH_SPICE!(dx, x, params, t)
         pos_3body /= params.DU
         dx[4:6] += third_body_accel(x[1:3], pos_3body, params.mus[i])
     end
-
-    if !isnothing(params.spherical_harmonics_data)
-        T_inr2pcpf = SPICE.pxform(params.naif_frame, params.frame_PCPF, params.et0 + t*params.TU)
-        a_SH = spherical_harmonics_accel(
-            T_inr2pcpf,
-            x[1:3] * params.DU,
-            params.spherical_harmonics_data["Cnm"],
-            params.spherical_harmonics_data["Snm"],
-            params.spherical_harmonics_data["GM"],
-            params.spherical_harmonics_data["REFERENCE RADIUS"],
-            params.spherical_harmonics_data["nmax"]
-        )
-        dx[4:6] += a_SH / (params.VU/params.TU)
-    end
-
+    
+    T_inr2pcpf = SPICE.pxform(params.naif_frame, params.frame_PCPF, params.et0 + t*params.TU)
+    a_SH = spherical_harmonics_accel(
+        T_inr2pcpf,
+        x[1:3] * params.DU,
+        params.spherical_harmonics_data["Cnm"],
+        params.spherical_harmonics_data["Snm"],
+        params.spherical_harmonics_data["GM"],
+        params.spherical_harmonics_data["REFERENCE RADIUS"],
+        params.spherical_harmonics_data["nmax"]
+    )
+    dx[4:6] += a_SH / (params.VU/params.TU)
     return nothing
 end
 
