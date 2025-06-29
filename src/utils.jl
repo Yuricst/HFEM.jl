667ffe8bd89fca49de504134e39be67dea2a9fcc
Yuri <yuri.shimane@gmail.com>
2025-06-28 23:40:04 -0400

    Append docstring

--- src/utils.jl
+++ src/utils.jl
@@ -7,11 +7,43 @@ function vector_hessian_forwarddiff(f::Function, x)
 end
 
 
+"""
+    eom_jacobian_fd(eom::Function, x, u, params, t)
+
+Evaluate Jacobian of equations of motion using ForwardDiff.
+The second argument `u` is a place-holder for control input.
+
+# Arguments
+- `eom`: "static" equations of motion, with signature `eom(x, params, t)` --> `dx`
+- `x`: State vector
+- `u`: Control input
+- `params`: Parameters
+- `t`: Time
+
+# Returns
+- `jac`: Jacobian of the equations of motion
+"""
 function eom_jacobian_fd(eom::Function, x, u, params, t)
     return ForwardDiff.jacobian(x -> eom(x, params, t), x)
 end
 
 
+"""
+    eom_hessian_fd(eom::Function, x, u, params, t)
+
+Evaluate Hessian of equations of motion using ForwardDiff.
+The second argument `u` is a place-holder for control input.
+
+# Arguments
+- `eom`: "static" equations of motion, with signature `eom(x, params, t)` --> `dx`
+- `x`: State vector
+- `u`: Control input
+- `params`: Parameters
+- `t`: Time
+
+# Returns
+- `hess`: Hessian of the equations of motion
+"""
 function eom_hessian_fd(eom::Function, x, u, params, t)
     return vector_hessian_forwarddiff(x -> eom(x, params, t), x)
 end
